b1c001582dbc2f5fbda13134ff49b3fbb945a959
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-12-18 11:41:30 +0100

    strip down object-model app

--- examples/object-model/app/models/song.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-const { attr } = DS;
-
-export default DS.Model.extend({
-  name:     attr('string'),
-  coverUrl: attr('string')
-});
--- examples/object-model/app/router.js
+++ examples/object-model/app/router.js
@@ -6,7 +6,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('songs');
 });
 
 export default Router;
