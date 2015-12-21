1be5a1253c48e50828ad2e89e7738068db4fd123
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-12-21 17:31:18 +0100

    add authentication example

---
+++ examples/authentication/server/mocks/token.js
@@ -0,0 +1,18 @@
+module.exports = function(app) {
+  var express     = require('express');
+  var tokenRouter = express.Router();
+
+  tokenRouter.post('/token', function(req, res) {
+    if (req.body.grant_type === 'password') {
+      if (req.body.username === 'letme' && req.body.password === 'in') {
+        res.status(200).send('{ "access_token": "secret token!", "account_id": 1 }');
+      } else {
+        res.status(400).send('{ "error": "invalid_grant" }');
+      }
+    } else {
+      res.status(400).send('{ "error": "unsupported_grant_type" }');
+    }
+  });
+
+  app.use('/', tokenRouter);
+};
