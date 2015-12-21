e19359cdfefe11ba775f2b028fa78e7685402f57
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-12-21 12:34:35 +0100

    add example for dependencies and ember addons

---
+++ examples/dependencies/ember-cli-build.js
@@ -0,0 +1,46 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  app.import({
+    development: 'bower_components/bootstrap/dist/css/bootstrap.css',
+    production:  'bower_components/bootstrap/dist/css/bootstrap.min.css'
+  });
+
+  app.import({
+    development: 'bower_components/font-awesome/css/font-awesome.css',
+    production:  'bower_components/font-awesome/css/font-awesome.min.css'
+  });
+
+  [
+    'FontAwesome.otf',
+    'fontawesome-webfont.svg',
+    'fontawesome-webfont.woff',
+    'fontawesome-webfont.eot',
+    'fontawesome-webfont.ttf',
+    'fontawesome-webfont.woff2'
+  ].forEach(function(font) {
+    var file = 'bower_components/font-awesome/fonts/' + font;
+    app.import(file, { destDir: 'fonts' });
+  });
+  
+  return app.toTree();
+};
