6809ca13118446eb9b7e57035635ce9d67514061
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-01-20 18:28:44 +0100

    make sure comments are loaded before player renders

--- tomster-player/app/routes/application.js
+++ tomster-player/app/routes/application.js
@@ -1,13 +1,17 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 const { RSVP } = Ember;
 
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('album').then((albums) => {
-      const songPromises = albums.invoke('get', 'songs');
+      const relationPromises = _.union(
+        albums.invoke('get', 'songs'),
+        albums.invoke('get', 'comments')
+      );
 
-      return RSVP.all(songPromises).then(() => albums)
+      return RSVP.all(relationPromises).then(() => albums)
     });
   }
 });
