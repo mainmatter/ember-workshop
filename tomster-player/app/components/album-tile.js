cdb8d83ae08f54d6359fbacc400ea0f3799860d7
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-01-20 20:32:51 +0100

    remove unnecessary class

--- tomster-player/app/components/album-tile.js
+++ tomster-player/app/components/album-tile.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['album-tile'],
-
   click() {
     const album = this.get('album');
     const onSelectAlbum = this.attrs['on-select-album'];
