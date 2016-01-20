85e89171e6a76849b9fb18de923c934b29eade2d
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-01-20 18:17:36 +0100

    no need to actually make a request on the single album page

--- tomster-player/app/controllers/index.js
+++ tomster-player/app/controllers/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     selectAlbum(album) {
-      this.transitionToRoute('album', album);
+      this.transitionToRoute('album', album.id);
     }
   }
 });
--- tomster-player/app/routes/album.js
+++ tomster-player/app/routes/album.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').find('album', params.album_id);
+    return this.get('store').peekRecord('album', params.album_id);
   }
 });
