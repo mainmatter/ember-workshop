2d7a881b75fba4483730f8031cd5b1f53323aa41
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-01-20 18:22:19 +0100

    make sure all songs are loaded on application route

--- tomster-player/app/routes/application.js
+++ tomster-player/app/routes/application.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
+const { RSVP } = Ember;
+
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('album');
+    return this.get('store').findAll('album').then((albums) => {
+      const songPromises = albums.invoke('get', 'songs');
+      return RSVP.all(songPromises).then(() => albums)
+    });
   }
 });
