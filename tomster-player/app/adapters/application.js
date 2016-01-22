ed6c50a1ac7ce07c13eefef353d62653c26a1948
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-01-22 13:29:44 +0100

    add authorization

--- tomster-player/app/adapters/application.js
+++ tomster-player/app/adapters/application.js
@@ -1,7 +1,9 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import config from '../config/environment';
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:oauth2-bearer',
   host: config.apiHost,
   namespace: 'api'
 });
