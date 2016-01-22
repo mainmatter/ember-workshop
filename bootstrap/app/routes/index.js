7fe2ad4d3676d137f2af95862af96950650368ed
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-01-22 13:39:22 +0100

    fix mock data

--- bootstrap/app/routes/index.js
+++ bootstrap/app/routes/index.js
@@ -1,38 +1,8 @@
 import Ember from 'ember';
+import data from '../lib/data';
 
 export default Ember.Route.extend({
   model() {
-    return [{
-      title: 'The Bodyguard',
-      'cover-url': 'https://i.scdn.co/image/7bcb439989b592287aeac1599aaa36be17672b73',
-      songs: [
-        { title: 'I Will Always Love You' },
-        { title: 'I Have Nothing' },
-        { title: "I'm Every Woman" },
-        { title: 'Run to You' },
-        { title: 'Queen of the Night - Radio Edit' },
-        { title: 'Jesus Loves Me' },
-        { title: 'Even If My Heart Would Break' },
-        { title: "Someday (I'm Coming Back)" },
-        { title: "It's Gonna Be A Lovely Day" },
-        { title: "What's So Funny 'Bout Peace, Love And Understanding" },
-        { title: 'Theme From The Bodyguard' }
-      ]
-    },
-    {
-      title: 'Whitney Houston',
-      'cover-url': 'https://i.scdn.co/image/7bcb439989b592287aeac1599aaa36be17672b73',
-      songs: [
-        { title: 'You Give Good Love' },
-        { title: 'Thinking About You' },
-        { title: 'Someone For Me' },
-        { title: 'Saving All My Love for You' },
-        { title: 'Nobody Loves Me Like You Do' },
-        { title: 'How Will I Know' },
-        { title: 'All at Once' },
-        { title: 'Take Good Care of My Heart' },
-        { title: 'Greatest Love of All' }
-      ]
-    }];
+    return data;
   }
 });
