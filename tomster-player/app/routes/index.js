842921c690b118db3141577f0b2147a25dd7367a
0x8890 <0x8890@airmail.cc>
2016-07-19 12:31:46 +0200

    authenticate index route

--- tomster-player/app/routes/index.js
+++ tomster-player/app/routes/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   beforeModel() {
     this.transitionTo('albums');
   }
