74983cc4b0856b9d8f344c96868b2b836d605dc8
0x8890 <0x8890@airmail.cc>
2016-07-19 10:20:58 +0200

    redirect index

--- tomster-player/app/controllers/albums/album/comment.js
+++ tomster-player/app/controllers/albums/album/comment.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     endCommenting(album) {
-      console.log(arguments)
       this.transitionToRoute('albums.album', album);
     }
   }
---
+++ tomster-player/app/routes/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend({
+  beforeModel() {
+    this.transitionTo('albums');
+  }
+});
