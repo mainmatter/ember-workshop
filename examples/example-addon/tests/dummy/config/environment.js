00fdd69839cfdad9428f74809c8139a52031d9d1
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-03 17:09:10 +0200

    update example addon

--- examples/example-addon/tests/dummy/config/environment.js
+++ examples/example-addon/tests/dummy/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'dependencies',
+    modulePrefix: 'dummy',
     environment,
     rootURL: '/',
     locationType: 'auto',
