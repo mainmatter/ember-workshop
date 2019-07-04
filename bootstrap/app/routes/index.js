9a786738ccdaae0c980f88d27aa3f3f587767c0d
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-04 16:46:51 +0200

    remove bootstrap app

--- bootstrap/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import data from '../lib/data';
-
-export default Ember.Route.extend({
-  model() {
-    return data;
-  }
-});
--- bootstrap/tests/helpers/phantom-shims.js
+++
@@ -1,5 +0,0 @@
-if (!String.prototype.includes) {
-  String.prototype.includes = function() {
-    return String.prototype.indexOf.apply(this, arguments) > -1;
-  };
-}
