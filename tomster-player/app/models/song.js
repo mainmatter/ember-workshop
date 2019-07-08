daaa87697a5f64effa5e4d159a6e5a25b0355ded
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-08 09:47:07 +0200

    step 3: ember data

---
+++ tomster-player/app/models/song.js
@@ -0,0 +1,11 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  title:    attr('string'),
+  duration: attr('number'),
+  mp3Url:   attr('string'),
+
+  album: belongsTo()
+});
