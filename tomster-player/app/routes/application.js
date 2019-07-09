791df40804d931c38107d1dd687d8e567084d849
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-09 11:58:25 +0200

    step 9: live data with websockets

--- tomster-player/app/routes/application.js
+++ tomster-player/app/routes/application.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject } from '@ember/service';
 
-export default Route.extend(ApplicationRouteMixin);
+export default Route.extend(ApplicationRouteMixin, {
+  session: inject(),
+  socket: inject(),
+
+  afterModel() {
+    if (this.session.isAuthenticated) {
+      this.socket.connect();
+    }
+  },
+	
+  sessionAuthenticated() {
+    this._super(...arguments);
+	
+    this.socket.connect();
+  },
+	
+  sessionInvalidated() {
+    this._super(...arguments);
+
+    this.socket.disconnect();
+  }
+});
---
+++ tomster-player/app/services/socket.js
@@ -0,0 +1,25 @@
+import Service from '@ember/service';
+import io from 'socket.io-client';
+import { inject } from '@ember/service';
+
+export default Service.extend({
+  store: inject(),
+  session: inject(),
+
+  connect() {
+    this.socket = io.connect('http://localhost:3000', {
+      query: {
+        token: this.session.data.authenticated.access_token
+      }
+    });
+    this.socket.on('comments:broadcast', (data) => this._pushNewComment(data));
+  },
+
+  disconnect() {
+    this.socket.disconnect();
+  },
+
+  _pushNewComment(data) {
+    this.store.pushPayload('comment', data);
+  }
+});
