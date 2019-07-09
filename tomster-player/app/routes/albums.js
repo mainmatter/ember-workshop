e360cfc632a63dc387121eed5d40e5402644531e
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-09 11:24:13 +0200

    step 8: authentication

--- tomster-player/app/routes/albums.js
+++ tomster-player/app/routes/albums.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class AlbumsRoute extends Route {
+  @service session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+
   async model() {
     let albums = await this.store.findAll('album');
 
