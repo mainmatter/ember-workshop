850558a087b2c432bd0bc087ccf0b375d8f5a51a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-11 20:42:06 +0200

    remove legacy action from example

--- examples/templates/app/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  actions: {
-    handleLegacyAction() {
-      alert('Handled the legacy action in the route!');
-    }
-  }
-});
