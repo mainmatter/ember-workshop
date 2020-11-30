d6072245a1a5a88834c5e4a537d21e9b2639397b
Patsy Issa <patsy.issa@gmail.com>
2020-11-30 18:33:13 +0100

    remove deprecated esa mixins

--- examples/auth/app/routes/application.js
+++ examples/auth/app/routes/application.js
@@ -1,6 +1,3 @@
 import Route from "@ember/routing/route";
-import ApplicationRouteMixin from "ember-simple-auth/mixins/application-route-mixin";
 
-export default class ApplicationRoute extends Route.extend(
-  ApplicationRouteMixin
-) {}
+export default class ApplicationRoute extends Route {}
