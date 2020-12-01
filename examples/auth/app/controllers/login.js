c24e1404ddf7fa66b5a3e344212a34efcdc2a850
Patsy Issa <patsy.issa@gmail.com>
2020-12-01 13:19:07 +0100

    Update auth example (#355)
    * update to native class
    
    * use simple auth 3.1.0-beta.1
    
    * remove deprecated esa mixins
    
    * add login redirect when authenticated
    
    * use angle bracket components
    
    * use modifiers for events
    
    * update ember infrastructure
    
    * remove setter
    
    * track error message
    
    * resolve lint errors
    
    * rename inject import to service

--- examples/auth/app/controllers/login.js
+++ examples/auth/app/controllers/login.js
@@ -1,16 +1,20 @@
-import Controller from '@ember/controller';
-import { inject } from '@ember/service';
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Controller from "@ember/controller";
+import { tracked } from "@glimmer/tracking";
 
-export default Controller.extend({
-  session: inject(),
+export default class LoginController extends Controller {
+  @service session;
+  @tracked errorMessage = null;
 
-  actions: {
-    authenticate(event) {
-      event.preventDefault();
+  @action
+  authenticate(event) {
+    event.preventDefault();
 
-      this.session.authenticate('authenticator:oauth2', this.identification, this.password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
+    this.session
+      .authenticate("authenticator:oauth2", this.identification, this.password)
+      .catch((reason) => {
+        this.errorMessage = reason.error || reason;
       });
-    }
   }
-});
+}
