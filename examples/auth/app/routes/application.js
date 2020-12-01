27d64c8e845e4f76c1fb2348a5fabe43e9716b63
Patsy Issa <patsy.issa@gmail.com>
2020-12-01 13:19:07 +0100

    Update auth example (#355)
    * update to native class
    
    * use simple auth 3.1.0-beta.1
    
    * remove deprecated esa mixins
    
    * add login redirect when authenticated
    
    * use angle bracket components
    
    * use modifiers for events
    
    * update ember infrastructure
    
    * remove setter
    
    * track error message
    
    * resolve lint errors
    
    * rename inject import to service

--- examples/auth/app/routes/application.js
+++
@@ -1,4 +0,0 @@
-import Route from '@ember/routing/route';
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-
-export default Route.extend(ApplicationRouteMixin);
