480e6d1afd86a620197de6164388f954c94693d4
Patsy Issa <patsy.issa@gmail.com>
2020-12-01 13:20:48 +0100

    update ember dependencies

--- examples/routing/app/router.js
+++ examples/routing/app/router.js
@@ -1,17 +1,15 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "routing/config/environment";
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
-Router.map(function() {
-  this.route('media', function() {
-    this.route('medium', { path: '/:medium_id' });
+Router.map(function () {
+  this.route("media", function () {
+    this.route("medium", { path: "/:medium_id" });
   });
-  this.route('fail');
-  this.route('not-found', { path: '*wildcard' });
+  this.route("fail");
+  this.route("not-found", { path: "*wildcard" });
 });
-
-export default Router;
