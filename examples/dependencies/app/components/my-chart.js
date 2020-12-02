ef1c670e05301ea6036a7a864bdd13199fa91455
Patsy Issa <patsy.issa@gmail.com>
2020-12-02 22:23:36 +0100

    update idioms

--- examples/dependencies/app/components/my-chart.js
+++ examples/dependencies/app/components/my-chart.js
@@ -1,12 +1,14 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 
 export default Component.extend({
   didRender() {
     this.highcharts.chart(this.element, {
-      series: [{
-        name: 'Some data',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-      }]
+      series: [
+        {
+          name: "Some data",
+          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+        },
+      ],
     });
-  }
+  },
 });
