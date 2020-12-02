7da8fce10403f0c687e5da0159b2cda8ab6891cb
Patsy Issa <patsy.issa@gmail.com>
2020-12-02 14:40:16 +0100

    update infrastructure

--- examples/component-composition/tests/test-helper.js
+++ examples/component-composition/tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'component-composition/app';
+import config from 'component-composition/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
