96f70cebeb09e2925467234cf611618938fc2be7
Patsy Issa <patsy.issa@gmail.com>
2020-12-03 11:35:24 +0100

    add smoke test

---
+++ examples/dependencies/tests/acceptance/smoke-test.js
@@ -0,0 +1,17 @@
+import { module, test } from "qunit";
+import { visit } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+
+module("Acceptance | smoke test", function (hooks) {
+  setupApplicationTest(hooks);
+
+  test("visiting /", async function (assert) {
+    await visit("/");
+    let faIcons = document.querySelectorAll(".font-awesome-paragraph svg");
+    assert.equal(faIcons.length, 3, "it renders the fa icons");
+
+    assert
+      .dom("[data-highcharts-chart]")
+      .exists("it initialises the highcharts chart");
+  });
+});
