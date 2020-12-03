695c0e7c231d1f5839ec8f8b400ade2b1e2e06c7
Patsy Issa <patsy.issa@gmail.com>
2020-12-03 11:11:27 +0100

    removed test selectors

--- examples/services/tests/acceptance/report-error-test.js
+++ examples/services/tests/acceptance/report-error-test.js
@@ -7,24 +7,24 @@ module("Acceptance | report error", function (hooks) {
 
   test("reporting an error", async function (assert) {
     await visit("/errors");
-    assert.dom("[data-test-error-count]").hasText("0 Error(s)");
+    assert.dom(".error-count").hasText("0 Error(s)");
 
-    await fillIn("[data-test-textarea]", "the roof is on fire");
-    await click("[data-test-submit-btn]");
+    await fillIn(".textarea", "the roof is on fire");
+    await click(".submit-btn");
 
-    assert.dom("[data-test-error-count]").hasText("1 Error(s)");
-    let errorItems = document.querySelectorAll("[data-test-error-item]");
+    assert.dom(".error-count").hasText("1 Error(s)");
+    let errorItems = document.querySelectorAll(".error-item");
     assert.equal(errorItems.length, 1);
     assert.dom(errorItems[0]).hasText("the roof is on fire");
 
-    assert.dom("[data-test-textarea]").hasText("");
+    assert.dom(".textarea").hasText("");
 
-    await fillIn("[data-test-textarea]", "we don't need no water");
-    await click("[data-test-submit-btn]");
+    await fillIn(".textarea", "we don't need no water");
+    await click(".submit-btn");
 
-    assert.dom("[data-test-error-count]").hasText("2 Error(s)");
-    errorItems = document.querySelectorAll("[data-test-error-item]");
-    assert.equal(document.querySelectorAll("[data-test-error-item]").length, 2);
+    assert.dom(".error-count").hasText("2 Error(s)");
+    errorItems = document.querySelectorAll(".error-item");
+    assert.equal(document.querySelectorAll(".error-item").length, 2);
     assert.dom(errorItems[1]).hasText("we don't need no water");
   });
 });
