322d822d1b665b6aa602d3addbe37d5b6e182db1
Patsy Issa <patsy.issa@gmail.com>
2020-12-03 15:21:04 +0100

    remove invalid generated test case

--- examples/component-composition/tests/unit/serializers/application-test.js
+++ examples/component-composition/tests/unit/serializers/application-test.js
@@ -1,23 +1,14 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Serializer | application', function(hooks) {
+module("Unit | Serializer | application", function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let serializer = store.serializerFor('application');
+  test("it exists", function (assert) {
+    let store = this.owner.lookup("service:store");
+    let serializer = store.serializerFor("application");
 
     assert.ok(serializer);
-  });
-
-  test('it serializes records', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let record = store.createRecord('application', {});
-
-    let serializedRecord = record.serialize();
-
-    assert.ok(serializedRecord);
   });
 });
