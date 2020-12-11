850cafb823f9275d438766d62dd495a7769eaa66
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-12-11 21:25:54 +0100

    fix socket server in web-sockets example

--- examples/web-sockets/server/socket.js
+++ examples/web-sockets/server/socket.js
@@ -1,16 +1,5 @@
 module.exports = function (app) {
-  var server = require("http").Server(app);
-  var io = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:4200",
-      methods: ["GET", "POST"],
-    },
-  });
-
-  server.listen(3000);
-  // eslint-disable-next-line no-console
-  console.log("Websocket server on http://localhost:3000");
-
+  const io = app.get("io");
   io.on("connection", function (socket) {
     socket.on("messaging", function (data) {
       io.sockets.emit("messaging", data);
