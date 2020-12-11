fb054ff374cf02a06f35260701b7caffd448955f
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-12-11 22:29:55 +0100

    send socket-id in header

--- tomster-player/app/adapters/application.js
+++ tomster-player/app/adapters/application.js
@@ -5,16 +5,18 @@ import ENV from 'tomster-player/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   @service session;
+  @service socket;
 
   host = ENV.apiHost;
   namespace = ENV.apiNamespace;
 
-  @computed('session.{isAuthenticated,data.authenticated.access_token}')
+  @computed('session.{isAuthenticated,data.authenticated.access_token}', 'socket.id')
   get headers() {
     let headers = {};
     if (this.session.isAuthenticated) {
       // OAuth 2
       headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+      headers['X-Socket-Id'] = this.socket.id;
     }
 
     return headers;
