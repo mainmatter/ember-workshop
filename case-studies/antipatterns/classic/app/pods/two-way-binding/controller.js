24f1d1298af0c191fe3c763cb41e1230df0e0db4
Daniel <daniel.kfir@sage.com>
2022-11-07 16:02:49 +0200

    fix

--- case-studies/antipatterns/classic/app/pods/two-way-binding/controller.js
+++ case-studies/antipatterns/classic/app/pods/two-way-binding/controller.js
@@ -1,6 +1,6 @@
-import Controller from '@ember/controller';
-import EmberObject from '@ember/object';
-import { alias } from '@ember/object/computed';
+import Controller from "@ember/controller";
+import EmberObject from "@ember/object";
+import { alias } from "@ember/object/computed";
 
 const Person = EmberObject.extend({
   name: null,
@@ -10,14 +10,14 @@ export default Controller.extend({
   person: null,
 
   init() {
-    this.set('person', Person.create({ name: 'John Doe'}));
+    this.set("person", Person.create({ name: "John Doe" }));
   },
 
-  name: alias('person.name'),
+  name: readOnly("person.name"),
 
   actions: {
     setName(event) {
-      this.set('name', event.target.value);
+      this.set("person.name", event.target.value);
     },
   },
 });
