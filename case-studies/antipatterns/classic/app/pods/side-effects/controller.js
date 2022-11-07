4fd93a82d6cb013d18f895df03e46f336a8238e6
Ilan Zaro <ilan.zaro@sage.com>
2022-11-07 15:34:24 +0200

    computed

--- case-studies/antipatterns/classic/app/pods/side-effects/controller.js
+++ case-studies/antipatterns/classic/app/pods/side-effects/controller.js
@@ -1,23 +1,26 @@
-import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import Controller from "@ember/controller";
+import { computed } from "@ember/object";
 
 export default Controller.extend({
-  firstName: 'John',
-  lastName: 'Doe',
+  firstName: "John",
+  lastName: "Doe",
 
   fullNameLength: null,
 
-  fullName: computed('firstName', 'lastName', function () {
-    const fullName = `${this.get('firstName')} ${this.get('lastName')}`;
-
-    this.set('fullNameLength', fullName.length);
-
+  fullName: computed("firstName", "lastName", function () {
+    const fullName = `${this.get("firstName")} ${this.get("lastName")}`;
     return fullName;
+  }),
+
+  fullNameLength: computed("fullName", function () {
+    if (this.get("fullName")) {
+      return this.get("fullName.length");
+    } else return null;
   }),
 
   actions: {
     change(property, event) {
       this.set(property, event.target.value);
-    }
+    },
   },
 });
