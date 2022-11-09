74240b33019bc8507c4b7cccc220dffa1da2c9cb
Daniel <daniel.kfir@sage.com>
2022-11-09 13:26:54 +0200

    fix

--- case-studies/antipatterns/classic/app/pods/leaking-state/component.js
+++ case-studies/antipatterns/classic/app/pods/leaking-state/component.js
@@ -1,10 +1,17 @@
-import Component from '@ember/component';
-import hbs from 'htmlbars-inline-precompile';
-import EmberObject from '@ember/object';
+import Component from "@ember/component";
+import hbs from "htmlbars-inline-precompile";
+import EmberObject from "@ember/object";
 
 export default Component.extend({
   // Component's tempalte is defined here to make exercise navigation easier.
   // This approach is OK for Ember integration tests, but don't use it in real life.
+  init() {
+    this._super(...arguments);
+    this.droid = EmberObject.create({
+      name: "R2D2",
+    });
+  },
+
   layout: hbs`
     Droid name:
 
@@ -14,15 +21,11 @@ export default Component.extend({
     />
   `,
 
-  tagName: 'label',
-
-  droid: EmberObject.create({
-    name: 'R2D2',
-  }),
+  tagName: "label",
 
   actions: {
     setName(event) {
-      this.set('droid.name', event.target.value);
-    }
+      this.set("droid.name", event.target.value);
+    },
   },
 });
