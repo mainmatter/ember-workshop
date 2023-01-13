eff0e865674c20c5c2331ee296a4db5e0b9196f1
Gabor Babicz <gabor@gaborbabicz.com>
2023-01-13 17:39:52 +0100

    Run ember-cli-update on component-composition (v3.27.0...v4.9.2)

--- examples/component-composition/tests/test-helper.js
+++ examples/component-composition/tests/test-helper.js
@@ -2,6 +2,10 @@ import Application from 'component-composition/app';
 import config from 'component-composition/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
