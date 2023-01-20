4850c2df9c22a8778e723292cab9afec5eb0e73d
Gabor Babicz <gabor@gaborbabicz.com>
2023-01-20 08:29:23 +0100

    Run ember-cli-update on routing (v3.27.0...v4.9.2)

--- examples/routing/tests/test-helper.js
+++ examples/routing/tests/test-helper.js
@@ -2,7 +2,10 @@ import Application from 'routing/app';
 import config from 'routing/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 
+setup(QUnit.assert);
 setApplication(Application.create(config.APP));
 
 start();
