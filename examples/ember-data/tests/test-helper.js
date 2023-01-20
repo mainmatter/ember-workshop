8d831fa9df2c352316a59cb44cf85f5c0fe5ce74
Gabor Babicz <gabor@gaborbabicz.com>
2023-01-20 08:12:10 +0100

    Run ember-cli-update on ember-data (v3.27.0...v4.9.2)

--- examples/ember-data/tests/test-helper.js
+++ examples/ember-data/tests/test-helper.js
@@ -4,7 +4,10 @@ import config from "ember-data/config/environment";
 /* eslint-enable ember/use-ember-data-rfc-395-imports */
 import { setApplication } from "@ember/test-helpers";
 import { start } from "ember-qunit";
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 
+setup(QUnit.assert);
 setApplication(Application.create(config.APP));
 
 start();
